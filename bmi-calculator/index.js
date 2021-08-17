580d9431940bfffc235b432c5476b632bb9bf837
Juan Carlos Orantes <72782842+Orantes-J@users.noreply.github.com>
2021-08-17 01:58:48 -0700

    Add files via upload

---
+++ bmi-calculator/index.js
@@ -0,0 +1,17 @@
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is amazing")
+
+const marksWeight = 78;
+const marksHeight = 1.69;
+
+const johnsWeight = 95;
+const johnsHeight = 1.76;
+
+const marksbmiCal = marksWeight/marksHeight ** 2;
+
+console.log(Math.ceil(marksbmiCal));
+
+const johnsbmiCal = johnsWeight/johnsHeight ** 2;
+
+console.log(Math.ceil(johnsbmiCal));
+
